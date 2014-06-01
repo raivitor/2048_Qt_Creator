0d743f92a290d77572b9b2d1eae10d5d03a1694a
raivitor2 <raivitor@gmail.com>
2014-06-01 16:30:13 -0300

    agora vai

---
+++ build-ITP-Desktop-Release/moc_mainwindow.cpp
@@ -0,0 +1,125 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../ITP/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[11];
+    char stringdata[79];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 13),
+QT_MOC_LITERAL(2, 25, 0),
+QT_MOC_LITERAL(3, 26, 10),
+QT_MOC_LITERAL(4, 37, 5),
+QT_MOC_LITERAL(5, 43, 10),
+QT_MOC_LITERAL(6, 54, 1),
+QT_MOC_LITERAL(7, 56, 11),
+QT_MOC_LITERAL(8, 68, 1),
+QT_MOC_LITERAL(9, 70, 1),
+QT_MOC_LITERAL(10, 72, 5)
+    },
+    "MainWindow\0keyPressEvent\0\0QKeyEvent*\0"
+    "event\0alterarCor\0n\0alterarTela\0i\0j\0"
+    "valor\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   29,    2, 0x08,
+       5,    1,   32,    2, 0x08,
+       7,    3,   35,    2, 0x08,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::QString, QMetaType::Int,    6,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Int,    8,    9,   10,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 1: { QString _r = _t->alterarCor((*reinterpret_cast< int(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
+        case 2: _t->alterarTela((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ build-ITP-Desktop-Release/ui_mainwindow.h
@@ -0,0 +1,287 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label11;
+    QLabel *label12;
+    QLabel *label13;
+    QLabel *label14;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label21;
+    QLabel *label22;
+    QLabel *label23;
+    QLabel *label24;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label31;
+    QLabel *label32;
+    QLabel *label33;
+    QLabel *label34;
+    QVBoxLayout *verticalLayout;
+    QLabel *label41;
+    QLabel *label42;
+    QLabel *label43;
+    QLabel *label44;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(592, 380);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(50, 30, 521, 261));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        label11 = new QLabel(layoutWidget);
+        label11->setObjectName(QStringLiteral("label11"));
+        label11->setFrameShape(QFrame::WinPanel);
+        label11->setFrameShadow(QFrame::Plain);
+        label11->setAlignment(Qt::AlignCenter);
+        label11->setMargin(0);
+
+        verticalLayout_4->addWidget(label11);
+
+        label12 = new QLabel(layoutWidget);
+        label12->setObjectName(QStringLiteral("label12"));
+        label12->setFrameShape(QFrame::WinPanel);
+        label12->setFrameShadow(QFrame::Plain);
+        label12->setAlignment(Qt::AlignCenter);
+        label12->setMargin(0);
+
+        verticalLayout_4->addWidget(label12);
+
+        label13 = new QLabel(layoutWidget);
+        label13->setObjectName(QStringLiteral("label13"));
+        label13->setFrameShape(QFrame::WinPanel);
+        label13->setFrameShadow(QFrame::Plain);
+        label13->setAlignment(Qt::AlignCenter);
+        label13->setMargin(0);
+
+        verticalLayout_4->addWidget(label13);
+
+        label14 = new QLabel(layoutWidget);
+        label14->setObjectName(QStringLiteral("label14"));
+        label14->setFrameShape(QFrame::WinPanel);
+        label14->setFrameShadow(QFrame::Plain);
+        label14->setAlignment(Qt::AlignCenter);
+        label14->setMargin(0);
+
+        verticalLayout_4->addWidget(label14);
+
+
+        horizontalLayout->addLayout(verticalLayout_4);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        label21 = new QLabel(layoutWidget);
+        label21->setObjectName(QStringLiteral("label21"));
+        label21->setFrameShape(QFrame::WinPanel);
+        label21->setFrameShadow(QFrame::Plain);
+        label21->setAlignment(Qt::AlignCenter);
+        label21->setMargin(0);
+
+        verticalLayout_2->addWidget(label21);
+
+        label22 = new QLabel(layoutWidget);
+        label22->setObjectName(QStringLiteral("label22"));
+        label22->setFrameShape(QFrame::WinPanel);
+        label22->setFrameShadow(QFrame::Plain);
+        label22->setAlignment(Qt::AlignCenter);
+        label22->setMargin(0);
+
+        verticalLayout_2->addWidget(label22);
+
+        label23 = new QLabel(layoutWidget);
+        label23->setObjectName(QStringLiteral("label23"));
+        label23->setFrameShape(QFrame::WinPanel);
+        label23->setFrameShadow(QFrame::Plain);
+        label23->setAlignment(Qt::AlignCenter);
+        label23->setMargin(0);
+
+        verticalLayout_2->addWidget(label23);
+
+        label24 = new QLabel(layoutWidget);
+        label24->setObjectName(QStringLiteral("label24"));
+        label24->setFrameShape(QFrame::WinPanel);
+        label24->setFrameShadow(QFrame::Plain);
+        label24->setAlignment(Qt::AlignCenter);
+        label24->setMargin(0);
+
+        verticalLayout_2->addWidget(label24);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        label31 = new QLabel(layoutWidget);
+        label31->setObjectName(QStringLiteral("label31"));
+        label31->setFrameShape(QFrame::WinPanel);
+        label31->setFrameShadow(QFrame::Plain);
+        label31->setAlignment(Qt::AlignCenter);
+        label31->setMargin(0);
+
+        verticalLayout_3->addWidget(label31);
+
+        label32 = new QLabel(layoutWidget);
+        label32->setObjectName(QStringLiteral("label32"));
+        label32->setFrameShape(QFrame::WinPanel);
+        label32->setFrameShadow(QFrame::Plain);
+        label32->setAlignment(Qt::AlignCenter);
+        label32->setMargin(0);
+
+        verticalLayout_3->addWidget(label32);
+
+        label33 = new QLabel(layoutWidget);
+        label33->setObjectName(QStringLiteral("label33"));
+        label33->setFrameShape(QFrame::WinPanel);
+        label33->setFrameShadow(QFrame::Plain);
+        label33->setAlignment(Qt::AlignCenter);
+        label33->setMargin(0);
+
+        verticalLayout_3->addWidget(label33);
+
+        label34 = new QLabel(layoutWidget);
+        label34->setObjectName(QStringLiteral("label34"));
+        label34->setFrameShape(QFrame::WinPanel);
+        label34->setFrameShadow(QFrame::Plain);
+        label34->setAlignment(Qt::AlignCenter);
+        label34->setMargin(0);
+
+        verticalLayout_3->addWidget(label34);
+
+
+        horizontalLayout->addLayout(verticalLayout_3);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        label41 = new QLabel(layoutWidget);
+        label41->setObjectName(QStringLiteral("label41"));
+        label41->setFrameShape(QFrame::WinPanel);
+        label41->setFrameShadow(QFrame::Plain);
+        label41->setAlignment(Qt::AlignCenter);
+        label41->setMargin(0);
+
+        verticalLayout->addWidget(label41);
+
+        label42 = new QLabel(layoutWidget);
+        label42->setObjectName(QStringLiteral("label42"));
+        label42->setFrameShape(QFrame::WinPanel);
+        label42->setFrameShadow(QFrame::Plain);
+        label42->setAlignment(Qt::AlignCenter);
+        label42->setMargin(0);
+
+        verticalLayout->addWidget(label42);
+
+        label43 = new QLabel(layoutWidget);
+        label43->setObjectName(QStringLiteral("label43"));
+        label43->setFrameShape(QFrame::WinPanel);
+        label43->setFrameShadow(QFrame::Plain);
+        label43->setAlignment(Qt::AlignCenter);
+        label43->setMargin(0);
+
+        verticalLayout->addWidget(label43);
+
+        label44 = new QLabel(layoutWidget);
+        label44->setObjectName(QStringLiteral("label44"));
+        label44->setFrameShape(QFrame::WinPanel);
+        label44->setFrameShadow(QFrame::Plain);
+        label44->setAlignment(Qt::AlignCenter);
+        label44->setMargin(0);
+
+        verticalLayout->addWidget(label44);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 592, 25));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        label11->setText(QApplication::translate("MainWindow", "2", 0));
+        label12->setText(QApplication::translate("MainWindow", "4", 0));
+        label13->setText(QApplication::translate("MainWindow", "8", 0));
+        label14->setText(QApplication::translate("MainWindow", "16", 0));
+        label21->setText(QApplication::translate("MainWindow", "32", 0));
+        label22->setText(QApplication::translate("MainWindow", "64", 0));
+        label23->setText(QApplication::translate("MainWindow", "128", 0));
+        label24->setText(QApplication::translate("MainWindow", "256", 0));
+        label31->setText(QApplication::translate("MainWindow", "512", 0));
+        label32->setText(QApplication::translate("MainWindow", "1024", 0));
+        label33->setText(QApplication::translate("MainWindow", "2048", 0));
+        label34->setText(QString());
+        label41->setText(QString());
+        label42->setText(QString());
+        label43->setText(QString());
+        label44->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
